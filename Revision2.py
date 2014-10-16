11ae174782afefd2cc7100f0f7dd0ddaafd93d00
Harry Derouich <Harry Derouich@HarryDerouichPC.home>
2014-10-16 19:44:31 +0100

    3rd Commit

--- Revision2.py
+++ Revision2.py
@@ -5,6 +5,7 @@
 message = input("Please enter a message: ")
 number = int(input("Please enter the number of times you want the message displayed: "))
 
+#This will print the message the amount of times they entered and was assigned as the variable number
 for count in range(number):
     print(message)
     
--- Revision3.py
+++ Revision3.py
@@ -2,9 +2,14 @@
 #14/10/14
 #Iteration Class Exercises - Revison 3
 
-number = int(input("Please enter the number of numbers ou want average"))
-
+number = int(input("Please enter the number of numbers you want average: "))
+avg_total = 0
 counter = 1
 while counter < (number + 1):
     avg_number = int("Please enter number {0} in the sequence".format(counter))
-    
+    avg_total = avg_total + avg_number
+    counter = counter + 1
+
+avg = avg_total / number
+
+print(avg)
