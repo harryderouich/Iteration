0e1579f98d30226ff84707feb86031f85cbcc8c9
harryderouich <40683@student.longroad.ac.uk>
2014-10-21 13:01:35 +0100

    Final commit - finished w/ new method

--- Revision6.py
+++ Revision6.py
@@ -2,17 +2,15 @@
 #17/10/14
 #Iteration Class Exercises - Revision 6
 
-counter = float(1)
-kilos = float(2.2 * counter)
+counter = 1
+kilos = 2.2 * counter
 
 
-print("|Pounds|Kilos|")
-while counter <= 10:
-    print("|  {0}   | {1} |".format(counter, kilos))
-    counter = counter + 1
-    kilos = 2.2 * counter
-while 10 < counter <= 20:
-    print("|  {0}  | {1} |".format(counter, kilos))
-    counter = counter + 1
-    kilos = 2.2 * counter
-    
+print("|{0:^7}|{1:^7}|".format("kilos","pounds"))
+while counter <= 20:
+        print("|{0:^7}|{1:^7.1f}|".format(counter, kilos))
+        counter = counter + 1
+        kilos = 2.2 * counter
+
+        
+
